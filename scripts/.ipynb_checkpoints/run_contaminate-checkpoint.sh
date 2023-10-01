49036a45e872f9471546047bf3d19afb10969594
AlbertoRosado1 <ar652820@ohio.edu>
2023-10-01 11:09:39 -0700

    version and ran arg no longer fixed

--- scripts/.ipynb_checkpoints/run_contaminate-checkpoint.sh
+++ scripts/.ipynb_checkpoints/run_contaminate-checkpoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# bash run_forward.sh
+# bash run_contaminate.sh ELG_LOP_complete_gtlimaging v0.6 15
 
 source /global/common/software/desi/desi_environment.sh main
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main 
 
-tracer=$1 #ELG_LOPnotqso
-version=v0.6
-nran=15
+tracer=$1 #ELG_LOP_complete_gtlimaging
+version=$2 # survey version to get SYSNet weights and hpmaps from
+nran=$3 # number of randoms to use 
 pydir=$HOME/sysgen2/py
 
 start_idx=0
